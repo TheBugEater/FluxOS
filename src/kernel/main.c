8788bf5aace578ffa6615a73dabf5af796b20b67
TheBugEater <dilhangeeth@hotmail.com>
2016-07-10 18:20:54 +0530

    Fixed Alignments in Code

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,17 +3,18 @@
 
 void kmain(void)
 {
-gdt_install();
-idt_install();
+    gdt_install();
+    idt_install();
 
-__asm__ __volatile__ ("sti");
+    __asm__ __volatile__ ("sti");
 
-terminal_initialize();
+    terminal_initialize();
 
-int d = 10;
-d = d/0;
-printf("Hello World! %d", d);
+    // Trigger a division Zero interupt
+    //
+    int d = 10;
+    d = d/0;
+    printf("Hello World! %d", d);
 
-for(;;);
-
+    for(;;);
 }
