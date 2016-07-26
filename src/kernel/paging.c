16286e9d3c02074bed88dc7e2560541af081135d
TheBugEater <dilhangeeth@hotmail.com>
2016-07-26 16:42:32 +0530

    Remove Commented Function Block

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -56,60 +56,8 @@ void install_paging(kernel_boot_info_t* info)
     add_page_mapping(p, a);
     *a = 10;
     printk("Value is %d",*a);
- }
-
-void install_paging_t(kernel_boot_info_t* info)
-{
-/*
-    add_interrupt_handler(14, page_fault_handler);
-    
-    // Page Directory and Page Table Takes 4mb for Full Address Space
-    // Reserve That
-    // Gives us 4096 bytes, which is enough to hold 1024 page tables
-    page_directory = (unsigned long*)new_block();
-    unsigned long* page_table = (unsigned long*)new_block();
-
-    unsigned long* v_page_directory = ADDR_TO_KERNEL_BASE(page_directory);
-    unsigned long* v_page_table = ADDR_TO_KERNEL_BASE(page_table);
-
-    printk("%x | %x\n",page_directory,page_table);
-    for(unsigned int i = 0; i < 1024; i++)
-    {
-        // Set the Address and Make it Present and Writable
-        v_page_table[i] = (i * 0x1000) | 3;
-    }
-
-    for(unsigned int i = 0; i < 1024; i++)
-    {
-        unsigned long* temp_page_table = (unsigned long*)new_block();
-        pde_t p_dir = create_new_pde_entry(0, temp_page_table);
-        v_page_directory[i] = *(unsigned long*)&p_dir;
-    }
-
-    v_page_directory[768] = ((unsigned long)page_table & 0xFFFFF000) | 3;
-    printk("%x\n",v_page_directory[768]);
- 
-    switch_page_directory(page_directory);
-
-    virtual_heap_top = KERNEL_HEAP_BASE;
-
-    printk("##########################\n");
-    printk("### TESTING ALLOCATION ###\n");
-    printk("##########################\n");
-    
-    int* a = (int*)0xAE50FFFF;
-    *a = 10;
-    *
-    for(int i=0; i<7000;i++)
-    {
-        void* p = (unsigned long)new_block() + KERNEL_VIRTUAL_BASE;
-        int a = *(int*)p;
-        a = 10;
-        printk("A = i x 10 = %d\n",a*i);
-    }
-*/ 
-
 }
+
 
 pte_t create_new_pte_entry(BOOL usr_mode, void* phys_addr)
 {
